bdfc267c1a38aa99182cd2629c546a0582352534
Ville <villekarjala@gmail.com>
2018-11-03 01:13:05 +0200

    4.4. add tests for total likes function

---
+++ 4-harkat/bloglist/tests/list_helper.test.js
@@ -0,0 +1,51 @@
+const listHelper = require('../utils/list_helper')
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
+    ]
+  
+    const emptyList = []
+
+    const listWithTwoBlogs = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+            _id: '5a422aa7ööö4a676234d17f8',
+            title: 'Hey Hey',
+            author: 'My my',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 1,
+            __v: 0
+          }
+      ]
+
+    test('when list has only one blog equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(5)
+    })
+
+    test('when list has no items at all result should be zero', () => {
+        const result = listHelper.totalLikes(emptyList)
+        expect(result).toBe(0)
+      })
+
+      test('when list has more than one item result should be a combination of all', () => {
+        const result = listHelper.totalLikes(listWithTwoBlogs)
+        expect(result).toBe(6)
+      })
+
+  })
--- 4-harkat/bloglist/utils/list_helper.js
+++ 4-harkat/bloglist/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
   }
   
+  const totalLikes = (blogs) => {
+    
+    let totalLikes = 0
+    for (var blog of blogs) {
+        totalLikes +=blog.likes
+      }
+      return totalLikes
+  }
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
