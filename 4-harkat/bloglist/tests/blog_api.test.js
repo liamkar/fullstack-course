3eb8d9dd7f8b6fa7621114c69431826514ba16c6
Ville <villekarjala@gmail.com>
2018-11-04 23:35:22 +0200

    4.8. add test for GET blogs

--- 4-harkat/bloglist/tests/blog_api.test.js
+++ 4-harkat/bloglist/tests/blog_api.test.js
@@ -3,22 +3,52 @@ const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
+const initialBlogs = [
+  {
+    author: 'Nick Cave',
+    title: 'Abattoir blues',
+    url:'www.nickcave.com',
+    votes: 66
+  },
+  {
+    author: 'Shane McGowan',
+    title: 'Rum sodomy and lash',
+    url:'www.shanemcgowan.com',
+    votes: 67
+  }
+]
+
 describe('API tests', () => {
-  /*
-  beforeAll(async () => {
-    await Note.remove({})
   
-    const noteObjects = initialNotes.map(note => new Note(note))
-    const promiseArray = noteObjects.map(note => note.save())
+  beforeAll(async () => {
+    await Blog.remove({})
+  
+    const blogObjects = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
     await Promise.all(promiseArray)
   })
-*/
 
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api
+      .get('/api/blogs')
+  
+    expect(response.body.length).toBe(initialBlogs.length)
+  })
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api
+      .get('/api/blogs')
+  
+    const contents = response.body.map(r => r.title)
+  
+    expect(contents).toContain('Rum sodomy and lash')
   })
 
   afterAll(() => {
