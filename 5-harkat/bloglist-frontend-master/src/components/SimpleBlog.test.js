184ef44874690509432f46be77825e2966670b92
Ville <villekarjala@gmail.com>
2018-11-14 20:21:37 +0200

    5.13. test front-end button clicks sandbox

--- 5-harkat/bloglist-frontend-master/src/components/SimpleBlog.test.js
+++ 5-harkat/bloglist-frontend-master/src/components/SimpleBlog.test.js
@@ -20,4 +20,29 @@ describe.only('<SimpleBlog />', () => {
     const votesDiv = simpleBlogComponent.find('.votes')
     expect(votesDiv.text()).toContain(blog.votes)
   })
+
+  it('clicking the button twice calls event handler twice', () => {
+    const blog = {
+        title: 'nabonabo',
+        author: 'Ville Totti',
+        url: 'www,test.com',
+        votes: 999,
+    }
+  
+    const mockHandler = jest.fn()
+  
+    const simpleBlogComponent = shallow(
+        <SimpleBlog 
+            blog={blog}
+            onClick={mockHandler}
+        />
+    )
+  
+    const button = simpleBlogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+  
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
+
