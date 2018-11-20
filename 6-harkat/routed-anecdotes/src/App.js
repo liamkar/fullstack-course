3578806a1626f2a9795be12f493fff9bbf50f762
Ville <villekarjala@gmail.com>
2018-11-20 22:26:19 +0200

    6.17 add single anecdote template and router

--- 6-harkat/routed-anecdotes/src/App.js
+++ 6-harkat/routed-anecdotes/src/App.js
@@ -14,10 +14,22 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} >
+        <Link to={`/anecdotes/${anecdote.id}`}>
+          {anecdote.content}</Link></li>)}
     </ul>  
   </div>
 )
+
+const Anecdote = ({anecdote}) => {
+  return(
+  <div>
+    <h2>{anecdote.content}</h2>
+    <div>{'has '+anecdote.votes+' votes'}</div>
+    <div>{'for more info see '+anecdote.url}</div>
+  </div>
+)}
+
 
 const About = () => (
   <div>
@@ -147,6 +159,7 @@ class App extends React.Component {
           <Route exact path="/" render={() => <AnecdoteList anecdotes={this.state.anecdotes}/>} />
           <Route path="/about" render={() => <About />} />
           <Route path="/create" render={() => <CreateNew addNew={this.addNew} />} />
+          <Route exact path="/anecdotes/:id" render={({match}) => <Anecdote anecdote={this.anecdoteById(match.params.id)} />}/>
           </div>
         </Router>
         <Footer />
